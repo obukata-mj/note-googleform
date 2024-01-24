0c328d39e14ee6b74d748dd901bac833808ec9ed
obukata <obukata@mj-star.jp>
2024-01-24 14:32:47 +0900

    noteユーザを変更

--- config/snip/noteapi.php
+++ config/snip/noteapi.php
@@ -1,5 +1,5 @@
 <?php
-	$note_creator  = 'mj_obu';
+	$note_creator  = 'mjtestnote';
 
 	$json = file_get_contents("https://note.com/api/v2/creators/{$note_creator}/contents?kind=note&page=1");
 	$json = mb_convert_encoding($json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
